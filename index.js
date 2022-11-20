a1ae66e2cbafd3fdfd53703d978936c6cbbc9f19
Chris Wamae <105814303+chris-wamae@users.noreply.github.com>
2022-11-20 21:33:03 +0300

    "Completed Assignment"

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function thankyouCards(array, event) {
+    const thankyouMessages = []
+    for (let i = 0; i < array.length; i++) {
+        thankyouMessages.push(`Thank you, ${array[i]},for the wonderful ${event} gift!`);
+    }
+    return thankyouMessages;
+}
+
+function countDown(number) {
+    while (number >= 0) { console.log(number--) }
+}
