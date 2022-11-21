56cb9c2e9cf839f5db107036321c3d34c59e97af
chris-wamae <chriswamae123@gmail.com>
2022-11-21 10:04:30 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+//create a function that when called and
+//given a multiple names and event returns
+//a thank you message with the name and event 
+//in an array.
+
+function writeCards(name,event)
+{const thankYous = []
+ for(let start = 0; start < name.length; start++ )
+ {thankYous.push(`Thank you, ${name[start]}, for the wonderful ${event} gift!`)}
+ return thankYous}
+
+function countDown(number) {
+    while (number >= 0) { console.log(number--) }
+}
